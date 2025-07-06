008631331d8cfbf15dd123d5a0ee63afdf653aed
Oleksii Mushtaiev <thascarfaze@gmail.com>
2025-07-07 00:11:45 +0400

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,32 @@
+class StringBuilder {
+  #value;
+
+  constructor(inititalValue) {
+    this.#value = inititalValue;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value += str;
+  }
+
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
